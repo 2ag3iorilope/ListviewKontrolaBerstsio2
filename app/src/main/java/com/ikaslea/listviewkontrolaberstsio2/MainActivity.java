d17ad98f57551eb3c6e755f56b03213db6f033d9
ioritzlope45@gmail.com <ioritzlope45@gmail.com>
2024-12-17 08:40:37 +0100

    Gridvierekin

--- app/src/main/java/com/ikaslea/listviewkontrolaberstsio2/MainActivity.java
+++ app/src/main/java/com/ikaslea/listviewkontrolaberstsio2/MainActivity.java
@@ -3,6 +3,7 @@ package com.ikaslea.listviewkontrolaberstsio2;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -24,34 +25,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView listViewIdiomas = findViewById(R.id.ListView1);
+
+        GridView gridViewIdiomas = findViewById(R.id.gridViewIdiomas);
         TextView textViewSeleccionado = findViewById(R.id.textViewSeleccionado);
+
 
         ArrayList<Hizkuntzak> hizkuntzak = new ArrayList<>();
         for (int i = 1; i <= 29; i++) {
-
             int nombreResId = getResources().getIdentifier("idioma_" + i, "string", getPackageName());
             int fraseResId = getResources().getIdentifier("frase_" + i, "string", getPackageName());
 
             if (nombreResId != 0 && fraseResId != 0) {
-
                 String nombre = getString(nombreResId);
                 String frase = getString(fraseResId);
-
                 hizkuntzak.add(new Hizkuntzak(nombre, frase));
             } else {
-
-                Log.e("MainActivity", "Errekurtsoa ez da aurkitu idioma_" + i + " o frase_" + i);
+                Log.e("MainActivity", "Errekurtsoa ez da aurkitu: Hizkuntza" + i + " edo Esaldi" + i);
             }
         }
 
+
         HizkuntzaAdapter adapter = new HizkuntzaAdapter(this, hizkuntzak);
-        listViewIdiomas.setAdapter(adapter);
+        gridViewIdiomas.setAdapter(adapter);
 
-        listViewIdiomas
-                .setOnItemClickListener((AdapterView<?> parent, android.view.View view, int position, long id) -> {
 
-                    textViewSeleccionado.setText(hizkuntzak.get(position).getFrase());
-                });
+        gridViewIdiomas.setOnItemClickListener((AdapterView<?> parent, android.view.View view, int position, long id) -> {
+            String frase = hizkuntzak.get(position).getFrase();
+            textViewSeleccionado.setText(frase);
+        });
+
+
+        textViewSeleccionado.setText("Ez da larunbata eta euria ari du");
     }
 }
